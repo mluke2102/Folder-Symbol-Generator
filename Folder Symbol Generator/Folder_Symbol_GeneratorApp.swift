dc99cca30f8628b199ff68e699a4a6941a65b681
Matthew Luke <mlukedev@icloud.com>
2023-04-20 18:17:30 -0400

    Initial Commit

---
+++ Folder Symbol Generator/Folder_Symbol_GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Folder_Symbol_GeneratorApp.swift
+//  Folder Symbol Generator
+//
+//  Created by Matt Luke on 4/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Folder_Symbol_GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
